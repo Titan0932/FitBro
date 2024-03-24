50f1b1467c62baf5997958002e2f22ea25923d0d
Titan0932 <anjishpradhananga@outlook.com>
2024-03-24 17:06:07 -0400

    added documentation

--- backend/db/userOperations.js
+++ backend/db/userOperations.js
@@ -1,6 +1,13 @@
 
 
 
+/**
+ * Inserts a user into the appropriate role table based on the provided role.
+ * @param {string} role - The role of the user (member, trainer, admin).
+ * @param {number} userId - The ID of the user.
+ * @throws {Error} If an invalid role is provided.
+ * @throws {Error} If there is an error registering the user.
+ */
 async function insertUserIntoRoleTable(role, userId) {
     try {
       if (role.toLowerCase() == "member") {
--- backend/utils/pw_Hashing.js
+++ backend/utils/pw_Hashing.js
@@ -2,6 +2,12 @@ const bcrypt = require('bcrypt');
 
 const saltRounds = 10;
 
+/**
+ * Generates a hash for the given password using bcrypt.
+ *
+ * @param {string} password - The password to be hashed.
+ * @returns {Promise<{getHasherr: string, hashedPw: string}>} A promise that resolves to an object containing the hash error (if any) and the hashed password.
+ */
 const getPwHash = (password) => {
     return new Promise((resolve, reject) => {
       bcrypt.hash(password, saltRounds, function(getHasherr, hashedPw) {
@@ -14,6 +20,12 @@ const getPwHash = (password) => {
     }
 )}
 
+/**
+ * Compares a password with a hash using bcrypt.
+ * @param {string} password - The password to compare.
+ * @param {string} hash - The hash to compare against.
+ * @returns {Promise<{err: Error, result: boolean}>} A promise that resolves with an object containing an error (if any) and the comparison result.
+ */
 const checkPwHash = (password, hash) => {
     return new Promise((resolve, reject) => {
       bcrypt.compare(password, hash, function(err, result) {
