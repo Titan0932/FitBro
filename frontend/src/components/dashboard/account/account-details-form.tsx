6ada4b04582a3759b8db30ac4bed60c9f4a954e9
Titan0932 <anjishpradhananga@outlook.com>
2024-04-02 00:11:08 -0400

    implemented userInfo updates

--- frontend/src/components/dashboard/account/account-details-form.tsx
+++ frontend/src/components/dashboard/account/account-details-form.tsx
@@ -14,6 +14,26 @@ import OutlinedInput from '@mui/material/OutlinedInput';
 import Select from '@mui/material/Select';
 import Grid from '@mui/material/Unstable_Grid2';
 
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Controller, useForm } from 'react-hook-form';
+import { z as zod } from 'zod';
+import { UserContext } from '@/contexts/user-context';
+import { FormHelperText } from '@mui/material';
+import { useRouter } from 'next/navigation';
+
+const schema = zod.object({
+  f_name: zod.string().min(1, { message: 'First name is required' }),
+  l_name: zod.string().min(1, { message: 'Last name is required' }),
+  email: zod.string().min(1, { message: 'Email is required' }).email(),
+  
+  phoneno: zod.string().optional(), // zod.string().optional().regex(/^\d{10}$/, 'Phone number must be 10 digits'),
+  city: zod.string().optional(),
+  state: zod.string().optional(),
+  country: zod.string().optional(),
+});
+
+type Values = zod.infer<typeof schema>;
+
 const states = [
   { value: 'alabama', label: 'Alabama' },
   { value: 'new-york', label: 'New York' },
@@ -21,12 +41,59 @@ const states = [
   { value: 'los-angeles', label: 'Los Angeles' },
 ] as const;
 
+const axios = require('axios');
+
 export function AccountDetailsForm(): React.JSX.Element {
+  const user = React.useContext(UserContext);
+  const [isPending, setIsPending] = React.useState<boolean>(false);
+  const router = useRouter();
+
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Values>({ defaultValues: user?.user ?? {}, resolver: zodResolver(schema) });
+
+  const onSubmit = async (values:Values) => {
+    setIsPending(true);
+    console.log("submittinggg -> ", values)
+    if(
+      values.f_name === user?.user?.f_name &&
+      values.l_name === user?.user?.l_name &&
+      values.phoneno === user?.user?.phoneno &&
+      values.city === user?.user?.city &&
+      values.state === user?.user?.state &&
+      values.country === user?.user?.country
+    ){
+      setIsPending(false);
+      return;
+    }else{
+      let config = {
+        method: 'put',
+        maxBodyLength: Infinity,
+        url: 'http://localhost:3005/users/updateUserInfo',
+        data: `f_name=${values.f_name}&l_name=${values.l_name}&email=${values.email}&phoneno=${values.phoneno}&city=${values.city}&state=${values.state}&country=${values.country}`,
+        headers: {"Authorization": "Bearer " + localStorage.getItem('custom-auth-token')}
+      };
+      await axios.request(config)
+      .then(async (response:any) => {
+        if(response){
+          console.log("Updated user info: ", response.data);
+          user?.setState((prev) => ({ ...prev, user: values }));
+        }
+      })
+      .catch((error:any) => {
+        console.log("Error: ", error);
+        // alert(error?.response?.data ?? error.message)
+      })
+      setIsPending(false);
+      router.refresh();
+    }
+  }
+
   return (
     <form
-      onSubmit={(event) => {
-        event.preventDefault();
-      }}
+      onSubmit={handleSubmit(onSubmit)}
     >
       <Card>
         <CardHeader subheader="The information can be edited" title="Profile" />
@@ -34,52 +101,96 @@ export function AccountDetailsForm(): React.JSX.Element {
         <CardContent>
           <Grid container spacing={3}>
             <Grid md={6} xs={12}>
-              <FormControl fullWidth required>
-                <InputLabel>First name</InputLabel>
-                <OutlinedInput defaultValue="Sofia" label="First name" name="firstName" />
-              </FormControl>
+              <Controller
+                control={control}
+                name="f_name"
+                render={({ field }) => (
+                  <FormControl fullWidth required error={Boolean(errors.f_name)}>
+                    <InputLabel>First name</InputLabel>
+                    <OutlinedInput {...field} defaultValue={user?.user?.f_name} label="First name" name="firstName" />
+                    {errors.f_name ? <FormHelperText>{errors.f_name.message}</FormHelperText> : null}
+                  </FormControl>
+                )}
+              />
             </Grid>
             <Grid md={6} xs={12}>
-              <FormControl fullWidth required>
-                <InputLabel>Last name</InputLabel>
-                <OutlinedInput defaultValue="Rivers" label="Last name" name="lastName" />
-              </FormControl>
+              <Controller
+                control={control}
+                name="l_name"
+                render={({ field }) => (
+                  <FormControl fullWidth required error={Boolean(errors.l_name)}>
+                    <InputLabel>Last name</InputLabel>
+                    <OutlinedInput {...field} defaultValue={user?.user?.l_name} label="Last name" name="lastName" />
+                    {errors.l_name ? <FormHelperText>{errors.l_name.message}</FormHelperText> : null}
+                  </FormControl>
+                )}
+              />
             </Grid>
             <Grid md={6} xs={12}>
-              <FormControl fullWidth required>
-                <InputLabel>Email address</InputLabel>
-                <OutlinedInput defaultValue="sofia@devias.io" label="Email address" name="email" />
-              </FormControl>
+              <Controller
+                control={control}
+                name="phoneno"
+                render={({ field }) => (
+                  <FormControl fullWidth>
+                    <InputLabel>Phone number</InputLabel>
+                    <OutlinedInput {...field} defaultValue={user?.user?.phoneno ?? ''} label="Phone number" name="phone" type="tel" />
+                    {errors.phoneno ? <FormHelperText>{errors.phoneno.message}</FormHelperText> : null}
+                  </FormControl>
+                )}
+              />
             </Grid>
             <Grid md={6} xs={12}>
-              <FormControl fullWidth>
-                <InputLabel>Phone number</InputLabel>
-                <OutlinedInput label="Phone number" name="phone" type="tel" />
-              </FormControl>
+              <Controller
+                control={control}
+                name="state"
+                render={({ field }) => (
+                  <FormControl fullWidth>
+                    <InputLabel>State</InputLabel>
+                    <Select {...field} defaultValue={user?.user?.state ?? ''} label="State" name="state" variant="outlined">
+                      {states.map((option) => (
+                        <MenuItem key={option.value} value={option.value}>
+                          {option.label}
+                        </MenuItem>
+                      ))}
+                    </Select>
+                    {errors.state ? <FormHelperText>{errors.state.message}</FormHelperText> : null}
+                  </FormControl>
+                )}
+              />
             </Grid>
             <Grid md={6} xs={12}>
-              <FormControl fullWidth>
-                <InputLabel>State</InputLabel>
-                <Select defaultValue="New York" label="State" name="state" variant="outlined">
-                  {states.map((option) => (
-                    <MenuItem key={option.value} value={option.value}>
-                      {option.label}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
+              <Controller
+                control={control}
+                name="city"
+                render={({ field }) => (
+                  <FormControl fullWidth>
+                    <InputLabel>City</InputLabel>
+                    <OutlinedInput {...field} defaultValue={user?.user?.city?? ''} label="City" />
+                    {errors.city ? <FormHelperText>{errors.city.message}</FormHelperText> : null}
+                  </FormControl>
+                )}
+              />
             </Grid>
             <Grid md={6} xs={12}>
-              <FormControl fullWidth>
-                <InputLabel>City</InputLabel>
-                <OutlinedInput label="City" />
-              </FormControl>
+              <Controller
+                control={control}
+                name="country"
+                render={({ field }) => (
+                  <FormControl fullWidth>
+                    <InputLabel>Country</InputLabel>
+                    <OutlinedInput {...field} defaultValue={user?.user?.country ?? ''} label="Country" />
+                    {errors.country ? <FormHelperText>{errors.country.message}</FormHelperText> : null}
+                  </FormControl>
+                )}
+              />
             </Grid>
           </Grid>
         </CardContent>
         <Divider />
         <CardActions sx={{ justifyContent: 'flex-end' }}>
-          <Button variant="contained">Save details</Button>
+          <Button disabled={isPending} type="submit" variant="contained">
+            Save details
+          </Button>
         </CardActions>
       </Card>
     </form>
--- frontend/src/components/dashboard/account/account-info.tsx
+++ frontend/src/components/dashboard/account/account-info.tsx
@@ -1,3 +1,4 @@
+'use client';
 import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
@@ -7,31 +8,36 @@ import CardContent from '@mui/material/CardContent';
 import Divider from '@mui/material/Divider';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
+import { UserContext } from '@/contexts/user-context';
 
-const user = {
-  name: 'Sofia Rivers',
-  avatar: '/assets/avatar.png',
-  jobTitle: 'Senior Developer',
-  country: 'USA',
-  city: 'Los Angeles',
-  timezone: 'GTM-7',
-} as const;
+// const user = {
+//   name: 'Sofia Rivers',
+//   avatar: '/assets/avatar.png',
+//   jobTitle: 'Senior Developer',
+//   country: 'USA',
+//   city: 'Los Angeles',
+//   timezone: 'GTM-7',
+// } as const;
 
 export function AccountInfo(): React.JSX.Element {
+  const user = React.useContext(UserContext);
   return (
     <Card>
       <CardContent>
         <Stack spacing={2} sx={{ alignItems: 'center' }}>
           <div>
-            <Avatar src={user.avatar} sx={{ height: '80px', width: '80px' }} />
+            <Avatar src={''} sx={{ height: '80px', width: '80px' }} />
           </div>
           <Stack spacing={1} sx={{ textAlign: 'center' }}>
-            <Typography variant="h5">{user.name}</Typography>
+            <Typography variant="h5">{`${user?.user?.f_name} ${user?.user?.l_name}`}</Typography>
             <Typography color="text.secondary" variant="body2">
-              {user.city} {user.country}
+              {user?.user?.city ?? "XXXX"} {user?.user?.country ?? "XXXX"}
             </Typography>
             <Typography color="text.secondary" variant="body2">
-              {user.timezone}
+              {user?.user?.user_dob}
+            </Typography>
+            <Typography color="text.secondary" variant="body2">
+              {user?.user?.email}
             </Typography>
           </Stack>
         </Stack>
