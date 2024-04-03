2d7a5bbf907ddcbe3cb0a2fe30dcb9d385dac285
Titan0932 <anjishpradhananga@outlook.com>
2024-04-02 23:28:42 -0400

    updating popover profile with user info

--- frontend/src/components/dashboard/layout/user-popover.tsx
+++ frontend/src/components/dashboard/layout/user-popover.tsx
@@ -16,6 +16,7 @@ import { paths } from '@/paths';
 import { authClient } from '@/lib/auth/client';
 import { logger } from '@/lib/default-logger';
 import { useUser } from '@/hooks/use-user';
+import { UserContext } from '@/contexts/user-context';
 
 export interface UserPopoverProps {
   anchorEl: Element | null;
@@ -25,7 +26,7 @@ export interface UserPopoverProps {
 
 export function UserPopover({ anchorEl, onClose, open }: UserPopoverProps): React.JSX.Element {
   const { checkSession } = useUser();
-
+  const user = React.useContext(UserContext);
   const router = useRouter();
 
   const handleSignOut = React.useCallback(async (): Promise<void> => {
@@ -57,9 +58,9 @@ export function UserPopover({ anchorEl, onClose, open }: UserPopoverProps): Reac
       slotProps={{ paper: { sx: { width: '240px' } } }}
     >
       <Box sx={{ p: '16px 20px ' }}>
-        <Typography variant="subtitle1">Sofia Rivers</Typography>
+        <Typography variant="subtitle1">{user?.user?.f_name + ' ' + user?.user?.l_name}</Typography>
         <Typography color="text.secondary" variant="body2">
-          sofia.rivers@devias.io
+          {user?.user?.email}
         </Typography>
       </Box>
       <Divider />
